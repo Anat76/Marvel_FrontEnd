e3855ae929c930c64549d21345fb97150ed0dcf8
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-11 17:42:45 +0200

    Create CharacterCard.jsx

---
+++ src/assets/components/CharacterCard.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const CharacterCard = ({ character }) => {
+  return (
+    character.thumbnail.path !==
+      "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
+    character.thumbnail.extension === "jpg" && (
+      <Link to={`/character/${character._id}`}>
+        <article key={character._id}>
+          <img
+            src={character.thumbnail.path + "." + character.thumbnail.extension}
+            alt={character.name}
+          />
+          <h2>{character.name}</h2>
+          <p>{character.description}</p>
+        </article>
+      </Link>
+    )
+  );
+};
+
+export default CharacterCard;
