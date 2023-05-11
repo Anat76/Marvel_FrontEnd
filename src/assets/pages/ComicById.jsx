04b98168e28883b9e1a5ad9d0b1dfddaf5fe59b6
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-11 22:27:01 +0200

    Update Comic's page

---
+++ src/assets/pages/ComicById.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const ComicById = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [comicIdData, setComicIdData] = useState();
+
+  const { id } = useParams();
+  //   console.log(id);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseData = await axios.get(
+          `http://localhost:3000/comic/${id}`
+        );
+        console.log(responseData.data);
+        setComicIdData(responseData.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <div>Loading . . .</div>
+  ) : (
+    <>
+      {comicIdData.thumbnail.path !==
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
+        comicIdData.thumbnail.extension === "jpg" && (
+          <img
+            src={
+              comicIdData.thumbnail.path + "." + comicIdData.thumbnail.extension
+            }
+            alt=""
+          />
+        )}
+      <div>
+        <h1>{comicIdData.title}</h1>
+        <p>{comicIdData.description}</p>
+      </div>
+    </>
+  );
+};
+export default ComicById;
--- src/assets/pages/Comics.jsx
+++ src/assets/pages/Comics.jsx
@@ -24,7 +24,7 @@ const Comics = () => {
     <p>Please Loading . . .</p>
   ) : (
     <>
-      <h1> je suis sur ma page "Comics" !</h1>
+      <h1> je suis sur la page du Comic : {comicsData.name} !</h1>
       {comicsData.results.map((comics) => {
         return <ComicsCard key={comics._id} comics={comics} />;
       })}
