79aceeca34361b9b1b5a8615cb177134e0adb0ef
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-05-11 17:43:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
-
+// Import utils React
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+// Import Pages
+import Characters from "./assets/pages/Characters";
+import CharacterById from "./assets/pages/CharacterById";
+import Comics from "./assets/pages/Comics";
+
+// Import Components
+import Header from "./assets/components/Header";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Characters />} />
+          <Route path="/character/:id" element={<CharacterById />} />
+          <Route path="/comics" element={<Comics />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
