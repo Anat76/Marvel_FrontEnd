5384dc144392ea0670a8fdef48b958180d57df32
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-06-01 21:41:20 +0200

    Update CharacterCard.jsx

--- src/assets/components/CharacterCard.jsx
+++ src/assets/components/CharacterCard.jsx
@@ -2,29 +2,25 @@ import { Link } from "react-router-dom";
 
 const CharacterCard = ({ character }) => {
   return (
-    character.thumbnail.path !==
-      "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
-    character.thumbnail.extension === "jpg" && (
-      <Link to={`/character/${character._id}`}>
-        <article key={character._id}>
-          <section>
-            <div>
-              <img
-                src={
-                  character.thumbnail.path + "." + character.thumbnail.extension
-                }
-                alt={character.name}
-              />
-            </div>
+    <Link to={`/character/${character._id}`}>
+      <article key={character._id}>
+        <section>
+          <div>
+            <img
+              src={
+                character.thumbnail.path + "." + character.thumbnail.extension
+              }
+              alt={character.name}
+            />
+          </div>
 
-            <div>
-              <h2>{character.name}</h2>
-              <p>{character.description}</p>
-            </div>
-          </section>
-        </article>
-      </Link>
-    )
+          <div>
+            <h2>{character.name}</h2>
+            <p>{character.description}</p>
+          </div>
+        </section>
+      </article>
+    </Link>
   );
 };
 
